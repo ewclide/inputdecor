ece20b16b3a12668962c55d49f424a3d3f01767f
ewclide <ewclide@yandex.ru>
2017-09-17 21:36:00 +0300

    full update script to es6 and return calendar

---
+++ index.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="jquery.js"></script>
+	<link rel="stylesheet" href="inputdecor.css">
+	<script src="inputdecor.js"></script>
+</head>
+<body>
+	<input type="checkbox" class="inputdecor"><br><br>
+	<input type="radio" name="radio" class="inputdecor" checked value="1">
+	<input type="radio" name="radio" class="inputdecor" value="2">
+	<input type="radio" name="radio" class="inputdecor" value="3">
+	<input type="radio" name="radio" class="inputdecor" value="4">
+	<input type="radio" name="radio" class="inputdecor"><br><br>
+	<select class="inputdecor" data-inputdecor-rollup="true" data-inputdecor-speed="150" >
+		<option value="1">value 1</option>
+		<option value="2">value 2</option>
+		<option value="3">value 3</option>
+		<option value="4">value 4</option>
+		<option value="5">value 5</option>
+	</select><br><br>
+	<input type="file" name="file">
+</body>
+</html>
--- inputdecor.js
+++ inputdecor.js
@@ -1,439 +1,246 @@
+'use strict'
+
 ;(function(){
-	function getUrlParams()
+
+	class Box
 	{
-		var search = location.search;
-		if(search)
+		constructor(){}
+
+		create(type)
 		{
-			result = {};
-			search = search.replace("?", "");
-			search = search.split("&");
-			search.forEach(function(p){
-				p = p.split("=");
-				result[p[0]] = p[1];
+			var self = this
+
+			this.elem.hide();
+
+			this.box = $(document.createElement("div"));
+			this.box.addClass("inputdecor-" + type);
+			this.elem.before(this.box);
+
+			this.button = $(document.createElement("div"));
+			this.button.addClass("button");
+			if (this.active) this.button.addClass("active");
+			this.box.append(this.button);
+
+			this.box.append(this.elem);
+
+			this.button.click(function(){
+				self.toogle();
 			});
-			return result;
-		}
-		else return false;
-	}
-
-	function setUrlParams(options)
-	{
-		var request = "?";
-		for (var i in options) request += i + "=" + options[i] + "&";
-			return request.slice(0, -1);
-	}
-
-	function Decorator(options)
-	{
-		this.init(options);
-	}
-	Decorator.prototype.init = function(options)
-	{
-		var type = options.elem.attr("type") || options.elem[0].tagName.toLowerCase();
-		if (type == "ul" || type == "select")
-			this.input = new Select(options);
-		else if (type == "checkbox")
-			this.input = new Checkbox(options);
-		else if (type == "calendar")
-			this.input = new Calendar(options);
-	}
-
-	function Calendar(options)
-	{
-		var self = this;
-		this.elem = options.elem;
-		this.active = false;
-		this.text = (function(){
-			var attr = self.elem.attr("data-inputdecor-text");
-			if (attr) return attr;
-			else return "Выберите дату";
-		})();
-		this.from = (function(){
-			var attr = self.elem.attr("data-inputdecor-from");
-			if (attr)
-			{
-				attr = attr.split(".");
-				return +new Date(attr[2], attr[1], attr[0]);
-			}
-			else return null;
-		})();
-		this.to = (function(){
-			var attr = self.elem.attr("data-inputdecor-to");
-			if (attr)
-			{
-				attr = attr.split(".");
-				return +new Date(attr[2], attr[1], attr[0]);
-			} 
-			else return null;
-		})();
-		this.init(options);
-	}
-	Calendar.prototype.init = function(options)
-	{
-		var self = this;
-
-		this.elem.hide();
-
-		var date = new Date();
-
-		this.now = {
-			year  : date.getFullYear(),
-			month : date.getMonth(),
-			day   : date.getDate()
 		}
 
-		this.used = {
-			year  : self.now.year,
-			month : self.now.month,
-			day   : self.now.day
-		}
-
-		this.box = $(document.createElement("div"));
-		this.box.addClass("inputdecor-box");
-		this.box.attr("id", this.elem.attr("id"));
-		this.elem.before(this.box);
-
-		this.calendar = $(document.createElement("div"));
-		this.calendar.addClass("inputdecor-calendar");
-
-		this.clear = $(document.createElement("button"));
-		this.clear.addClass("inputdecor-clear");
-		this.clear.text("Очистить");
-
-		this.apply = $(document.createElement("button"));
-		this.apply.addClass("inputdecor-apply");
-		this.apply.text("Применить");
-
-		this.button = $(document.createElement("p"));
-		this.button.addClass("inputdecor-button");
-		this.button.text(this.text);
-		this.box.append(this.button);
-
-		//-------------------------------------------------------
-		this.monthesList = $(document.createElement("select"));
-		var monthesList = "";
-		var monthes = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-		monthes.forEach(function(month, i){
-			var selected = "";
-			if (self.now.month == i) selected = "selected";
-			monthesList += '<option value="' + i + '" ' + selected + '>' + month + '</option>';
-		});
-		this.monthesList.append(monthesList);
-		this.monthesList.change(function(){
-			self.build({ month : $(this).val() });
-		});
-		this.calendar.append(this.monthesList);
-
-		//-------------------------------------------------------
-		this.yearsList = $(document.createElement("select"));
-		var yearsList = "";
-		for (var i = this.now.year; i >= this.now.year - 17; i--)
+		activate()
 		{
-			var selected = "";
-			if (i == this.now.year) selected = "selected";
-			yearsList += '<option value="' + i + '" ' + selected + '>' + i + '</option>';
+			this.button.addClass("active");
+			this.elem.prop({ 'checked': true });
+			this.active = true;
 		}
-		this.yearsList.append(yearsList);
-		this.yearsList.change(function(){
-			self.build({ year : $(this).val() });
-		});
-		this.calendar.append(this.yearsList);
-		//-------------------------------------------------------
 
-		this.table = $(document.createElement("table"));
-		this.table[0].onselectstart = function(){ return false; };
-		this.calendar.append(this.table);
+		deactivate()
+		{
+			this.button.removeClass("active");
+			this.elem.prop({ 'checked': false });
+			this.active = false;
+		}
 
-		this.box.append(this.calendar);
-		this.calendar.append(this.clear);
-		this.calendar.append(this.apply);
+		toogle()
+		{
+			if (this.active) this.deactivate();
+			else this.activate();
+		}
+	}
 
-		this.elem.remove();
+	class Decorator
+	{
+		constructor(options)
+		{
+    		var type = options.elem.attr("type") || options.elem[0].tagName.toLowerCase();
+			if (type == "ul" || type == "select")
+				this.input = new Select(options);
+			else if (type == "checkbox")
+				this.input = new Checkbox(options);
+			else if (type == "radio")
+				this.input = new Radio(options);
+			else this.input = new Checkbox(options);
+  		}
+	}
 
-		this.clear.click(function(e){
-			self.from = null;
-			self.to = null;
-			self.button.text(self.text);
-			self.build(self.used);
-		});
+	class Checkbox extends Box
+	{
+		constructor(options)
+		{
+			super();
+			this.init(options);
+			super.create("checkbox");
+		}
 
-		this.apply.click(function(e){
-			self.appendDate();	
-			if(self.from && self.to)
-			{
-				var from = self.getTextDate(self.from);
-				var to = self.getTextDate(self.to);
+		init(options)
+		{
+			var self = this;
+			this.elem = options.elem;
+			this.name = options.elem.attr("name");
+			this.value = options.elem.val();
+			this.active = (function(){
+				var checked = self.elem.prop("checked") || self.elem.attr("checked");
+				if (checked) return true;
+				else return false;
+			})();
+		}
+	}
 
-				var loc = location.pathname;
-				var url = getUrlParams();
-				if (url)
-				{
-					url.from = from;
-					url.to = to;
-					loc += setUrlParams(url);
-				}
-				else loc += setUrlParams({ from : from, to : to });
+	class Radio extends Box
+	{
+		constructor(options)
+		{
+			super();
+			this.init(options);
+			super.create("radio");
+		}
 
-				location.href = loc;
-			}
-			else
-			{
-				var url = getUrlParams();
-				delete url.from;
-				delete url.to;
-				location.href = location.pathname + setUrlParams(url);
-			}
-		});
+		init(options)
+		{
+			var self = this;
+			this.elem = options.elem;
+			this.name = options.elem.attr("name");
+			this.value = options.elem.val();
+			this.active = (function(){
+				var checked = self.elem.prop("checked") || self.elem.attr("checked");
+				if (checked) return true;
+				else return false;
+			})();
+			this.radios = $('input[type=radio][name="' + this.name + '"]');
+			this.elem[0].inputdecor = this;
+		}
 
-		this.table.click(function(e){
-			var day = parseInt(e.target.innerText);
-			if (day)
-			{
-				var current = +new Date(self.used.year, self.used.month, day);
-				if (!self.from)
-				{
-					self.from = current;
-				}
-				else if (!self.to)
-				{
-					self.to = current;
-					self.build(self.used);
-				}
+		deactiveOther(current)
+		{
+			this.radios.each(function(){
+				if (this.inputdecor !== current)
+					this.inputdecor.deactivate();
+			});
+		}
+
+		activate()
+		{
+			super.activate();
+			this.deactiveOther(this);
+		}
+	}
+
+	class Select
+	{
+		constructor(options)
+		{
+			this.init(options);
+		}
+
+		init(options)
+		{
+			var self = this;
+			this.elem = options.elem;
+			this.name = options.elem.attr("name");
+			this.value = options.elem.val();
+			this.speed = parseInt(this.elem.attr("data-inputdecor-speed")) || 250;
+			this.rollup = self.elem.attr("data-inputdecor-rollup") || false;
+			this.active = false;
+			this.text = (function(){
+				var text = "Выберите из списка";
+				var active = self.elem.find("li.active");
+				if (active.length) text = active.text();
 				else
 				{
-					var center = (self.to - self.from)/2;
-					if (current - self.from < center) self.from = current;
-					else self.to = current;
-					self.build(self.used);
+					var attr = self.elem.attr("data-inputdecor-text");
+					if (attr) text = attr;
 				}
-			}
-		});
-
-		this.button.click(function(){
-			self.toogle();
-		});
-
-		this.appendDate();
-		this.build(this.now);
-	}
-	Calendar.prototype.appendDate = function()
-	{
-		if (this.from && this.to)
-		{
-			var from = this.getTextDate(this.from);
-			var to = this.getTextDate(this.to);
-
-			this.button.text(from + "-" + to);
-			this.close();
+				return text;
+			})();
+			this.class = this.elem.attr("data-inputdecor-class") || "";
+			this.create();
 		}
-	}
-	Calendar.prototype.getTextDate = function(num)
-	{
-		var date = new Date(num);
-		var result = date.getDate() + ".";
-			result += date.getMonth() + ".";
-			result += date.getFullYear();
-		return result;
-	}
-	Calendar.prototype.build = function(d)
-	{
 
-		if (d.year) this.used.year = d.year;
-		if (d.month) this.used.month = d.month;
-		if (d.day) this.used.day = d.day;
-
-		var date = new Date(this.used.year, this.used.month);
-
-		var body = '<tr class="title"><td>пн</td><td>вт</td><td>ср</td><td>чт</td><td>пт</td><td>сб</td><td>вс</td></tr><tr>';
-
-		for (var i = 0; i < getDay(date); i++) body += '<td></td>';
-
-		while (date.getMonth() == this.used.month)
+		create()
 		{
-			var curDay = date.getDate();
+			var self = this;
 
-			var className = "default";
-			if (this.used.year == this.now.year && this.used.month == this.now.month && curDay == this.now.day) className = "today";
+			this.elem.hide();
 
-			if (this.from && this.to)
+			this.select = $(document.createElement("div"));
+			this.select.addClass("inputdecor-select");
+			if (this.class) this.select.addClass(this.class);
+			this.elem.before(this.select);
+
+			this.button = $(document.createElement("button"));
+			this.button.addClass("button");
+			this.button.text(this.text);
+			this.select.append(this.button);
+
+			this.label = $(document.createElement("span"));
+			this.label.addClass("label");
+			this.select.append(this.label);
+
+			this.hidden = $(document.createElement("input"));
+			this.hidden.attr("type", "hidden");
+			if (this.name) this.hidden.attr("name", this.name);
+			this.hidden.val(this.value);
+			this.select.append(this.hidden);
+
+			this.list = $(document.createElement("ul"));
+			this.list.addClass("list");
+			var list = this.elem[0].innerHTML;
+				list = list.replace(/option+/g, "li");
+			this.list.append('<li>'+this.text+'</li>');
+			this.list.append(list);
+			this.select.append(this.list);
+
+			if (this.rollup)
 			{
-				var current = +new Date(this.used.year, this.used.month, curDay);
-				if (current > this.from && current < this.to) className += " range";
-				if (current == this.from || current == this.to) className += " limit";
+				this.rollup = $(document.createElement("span"));
+				this.rollup.addClass("rollup");
+				this.list.append(this.rollup);
+				this.rollup.click(function(e){
+					self.close();
+				});
 			}
 
-			body += '<td class="' + className + '">' + curDay + '</td>';
-			if (getDay(date) % 7 == 6) body += '</tr><tr>';
-			date.setDate(curDay + 1);
+			this.elem.remove();
+
+			this.button.click(function(e){
+				self.toogle();
+			});
+
+			this.list.click(function(e){
+				if (e.target.tagName == "LI")
+				{
+					var target = $(e.target);
+					self.hidden.val(target.attr("value"));
+					self.button.text(target.text());
+				}
+			});
+
+			document.body.addEventListener("click", function(e){
+				if (e.target.parentNode != self.select[0]) self.close();
+			});
 		}
 
-		if (getDay(date) != 0)
-		for (var i = getDay(date); i < 7; i++) body += '<td></td>';
-
-		body += '</tr>'
-
-		this.table.empty();
-		this.table.append(body);
-		
-		function getDay(date)
+		open()
 		{
-			var day = date.getDay();
-			if (day == 0) day = 7;
-			return day - 1;
+			this.list.show(this.speed);
+			this.label.addClass("active");
+			this.active = true;
 		}
-	}
-	Calendar.prototype.open = function()
-	{
-		this.calendar.show(250);
-		this.active = true;
-	}
-	Calendar.prototype.close = function()
-	{
-		this.calendar.hide(250);
-		this.active = false;
-	}
-	Calendar.prototype.toogle = function()
-	{
-		if (this.active) this.close();
-		else this.open();
-	}
 
-	function Checkbox(options)
-	{
-		var self = this;
-		this.elem = options.elem;
-		this.name = options.elem.attr("name");
-		this.value = options.elem.val();
-		this.active = (function(){
-			var checked = self.elem.prop("checked") || self.elem.attr("checked");
-			if (checked) return true;
-			else return false;
-		})();
-		this.create();
-	}
-	Checkbox.prototype.create = function()
-	{
-		var self = this
+		close()
+		{
+			this.list.hide(this.speed);
+			this.label.removeClass("active");
+			this.active = false;
+		}
 
-		this.elem.hide();
+		toogle()
+		{
+			if (this.active) this.close();
+			else this.open();
+		}
 
-		this.box = $(document.createElement("div"));
-		this.box.addClass("inputdecor-box");
-		this.elem.before(this.box);
-
-		this.button = $(document.createElement("div"));
-		this.button.addClass("inputdecor-checkbox");
-		if (this.active) this.button.addClass("active");
-		this.box.append(this.button);
-
-		this.box.append(this.elem);
-
-		this.button.click(function(){
-			self.toogle();
-		});
-	}
-	Checkbox.prototype.activate = function()
-	{
-		this.button.addClass("active");
-		this.elem.prop({ 'checked': true });
-		this.active = true;
-	}
-	Checkbox.prototype.deactivate = function()
-	{
-		this.button.removeClass("active");
-		this.elem.prop({ 'checked': false });
-		this.active = false;
-	}
-	Checkbox.prototype.toogle = function()
-	{
-		if (this.active) this.deactivate();
-		else this.activate();
-	}
-
-	function Select(options)
-	{
-		var self = this;
-		this.elem = options.elem;
-		this.name = options.elem.attr("name");
-		this.value = options.elem.val();
-		this.active = false;
-		this.text = (function(){
-			var text = "Выберите из списка";
-			var active = self.elem.find("li.active");
-			if (active.length) text = active.text();
-			else
-			{
-				var attr = self.elem.attr("data-inputdecor-text");
-				if (attr) text = attr;
-			}
-			return text;
-		})();
-		this.create();
-	}
-	Select.prototype.create = function()
-	{
-		var self = this;
-
-		this.elem.hide();
-
-		this.box = $(document.createElement("div"));
-		this.box.addClass("inputdecor-box");
-		this.elem.before(this.box);
-
-		this.button = $(document.createElement("p"));
-		this.button.addClass("inputdecor-button");
-		this.button.text(this.text);
-		this.box.append(this.button);
-
-		this.label = $(document.createElement("span"));
-		this.label.addClass("inputdecor-label");
-		this.box.append(this.label);
-
-		this.hidden = $(document.createElement("input"));
-		this.hidden.attr("type", "hidden");
-		if (this.name) this.hidden.attr("name", this.name);
-		this.hidden.val(this.value);
-		this.box.append(this.hidden);
-
-		this.list = $(document.createElement("ul"));
-		this.list.addClass("inputdecor-list");
-		var list = this.elem[0].innerHTML;
-			list = list.replace(/option+/g, "li");
-		this.list.append(list);
-		this.box.append(this.list);
-
-		this.elem.remove();
-
-		this.button.click(function(){
-			self.toogle();
-		});
-
-		this.list.click(function(e){
-			var target = $(e.target);
-			self.hidden.val(target.attr("value"));
-			self.button.text(target.text());
-		});
-
-		document.body.addEventListener("click", function(e){
-			if (e.target.parentNode != self.box[0]) self.close();
-		});
-	}
-	Select.prototype.open = function()
-	{
-		this.list.show(250);
-		this.label.addClass("active");
-		this.active = true;
-	}
-	Select.prototype.close = function()
-	{
-		this.list.hide(250);
-		this.label.removeClass("active");
-		this.active = false;
-	}
-	Select.prototype.toogle = function()
-	{
-		if (this.active) this.close();
-		else this.open();
 	}
 
 	$.fn.decorate = function(options)
